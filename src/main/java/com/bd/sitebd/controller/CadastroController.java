f64f88d743c9ce2e62d49ae678b7c90cddd868ea
Cauã Carvalho <CauCauPlays@gmail.com>
2025-06-17 15:07:13 -0300

    Colocando CSS nas páginas

--- src/main/java/com/bd/sitebd/controller/CadastroController.java
+++ src/main/java/com/bd/sitebd/controller/CadastroController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 import com.bd.sitebd.model.Cliente;
 import com.bd.sitebd.service.ClienteService;
